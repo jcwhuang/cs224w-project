324a7bbf0cd1a8b0a381b029bb6f1266fefb95d3
jcwhuang <jcwhuang@gmail.com>
2015-11-11 12:30:02 -0800

    QLearning and simulate runs, though buggy

--- fantasyMDP.py
+++ fantasyMDP.py
@@ -42,14 +42,21 @@ class ComputeRosterMDP(util.MDP):
 	# 5) budget = 100million
 	# 6) Can select up to 3 players from a single Premier League team
 	def actions(self,state):
-		actions = []		
-		for p in self.allPlayers:
-			if p not in state[0]: #if not in roster yet
-				positionConstraint = (state.positions[p.position] < self.maxPositions[p.position])
-				budgetConstraint = (p.price < state.budget)
-				teamConstraint = (state.teams[p.team] < 3)
-				if positionConstraint and budgetConstraint and teamConstraint:
-					actions.append(p)
+		# actions = []		
+		# stateRoster, stateBudget, statePositions, stateTeams = state
+		# for playerName in self.allPlayers:
+		# 	p = self.allPlayers[playerName]
+		# 	if p not in state[0]: #if not in roster yet
+		# 		positionConstraint = (statePositions[p.position] < self.maxPositions[p.position])
+		# 		budgetConstraint = (p.price < stateBudget)
+		# 		teamConstraint = (stateTeams[p.team] < 3)
+		# 		if positionConstraint and budgetConstraint and teamConstraint:
+		# 			actions.append(p)
+
+		# TO DO: experiment. Just always return all possible players
+		#        satisfy constraints in succAndProbReward
+		#        just like we did in blackjack
+		actions = [self.allPlayers[playerName] for playerName in self.allPlayers]
 		return actions
 
 	def succAndProbReward(self, state, action):
@@ -57,82 +64,95 @@ class ComputeRosterMDP(util.MDP):
 		if len(state[0]) == 15:
 			return []
 
+		validPlayer = False
+		stateRoster, stateBudget, statePositions, stateTeams = state
+		if action not in state[0]: # if not in roster yet
+			positionConstraint = (statePositions[p.position] < self.maxPositions[p.position])
+			budgetConstraint = (p.price < stateBudget)
+			teamConstraint = (stateTeams[p.team] < 3)
+			if positionConstraint and budgetConstraint and teamConstraint:
+				validPlayer = True
+		
+		if not validPlayer:
+			return []
+
 		# compute new state
 		newRoster = state[0]
 		newRoster.append(action)
 		newBudget = state[1]-action.price
 		newPositions = state[2]
-		newPosition[action.position] += 1
+		newPositions[action.position] += 1
 		newTeams = state[3]
 		newTeams[action.team] += 1
 		newState = (newRoster, newBudget, newPositions, newTeams)
 
 		# TO DO: Finish reward function
-		reward = 0 
-		goalsScoredWeight = 0
-		if action.position == "GK" or action.position == "DEF":
-			goalsScoredWeight = 6
-		elif action.position == "MID":
-			goalsScoredWeight = 5
-		else:
-			goalsScoredWeight = 4
+		# reward = 0 
+		# goalsScoredWeight = 0
+		# if action.position == "GK" or action.position == "DEF":
+		# 	goalsScoredWeight = 6
+		# elif action.position == "MID":
+		# 	goalsScoredWeight = 5
+		# else:
+		# 	goalsScoredWeight = 4
 
-		weights={'playedInGame': 1, 'played60Min': 2, 'goalsScored': goalsScoredWeight, \
-			'penaltiesReceived' : 1, 'penaltiesConceded': -1, \
-			'goalsConceded' : -1, 'yellowCard' : -1, 'redCard':-3, \
-			'savesByGK': 1}
+		# weights={'playedInGame': 1, 'played60Min': 2, 'goalsScored': goalsScoredWeight, \
+		# 	'penaltiesReceived' : 1, 'penaltiesConceded': -1, \
+		# 	'goalsConceded' : -1, 'yellowCard' : -1, 'redCard':-3, \
+		# 	'savesByGK': 1}
 
-		# commented out features that we may not have stats for
-		# TO DO: some of these are in team stats. Should we use that?
+		# # commented out features that we may not have stats for
+		# # TO DO: some of these are in team stats. Should we use that?
 
-		playedInGame = False 	# 1pt, M > 0
-		played60Min = False		# 2pt, M >= 60
-		goalsScored = 0			# 6pt if GK or DEF, 5pt if MID, 4 if STR, G & position
-		# assists = 0				# 3pt
-		# noScoreOnGK = False		# 4pt if played 60min
-		# noScoreOnDEF = False	# 4pt if played 60min
-		# noScoreOnMID = False	# 1pt if played 60min
-		penaltiesReceived = 0	# 1pt, FS?
-		penaltiesConceded = 0	# -1pt, FC?
-		# penaltyMisses = 0		# -2pt
-		# penaltySaves = 0		# 5pt
-		# goalsConceded = 0		# -1pt/2goals for GK or MID
-		yellowCard = False		# -1pt, Y
-		redCard = False			# -3pt, R
-		savesByGK = 0			# 1pt/3saves, AB & position is GK
-		# recoveredBalls = 0		# 1pt/5balls
+		# playedInGame = False 	# 1pt, M > 0
+		# played60Min = False		# 2pt, M >= 60
+		# goalsScored = 0			# 6pt if GK or DEF, 5pt if MID, 4 if STR, G & position
+		# # assists = 0				# 3pt
+		# # noScoreOnGK = False		# 4pt if played 60min
+		# # noScoreOnDEF = False	# 4pt if played 60min
+		# # noScoreOnMID = False	# 1pt if played 60min
+		# penaltiesReceived = 0	# 1pt, FS?
+		# penaltiesConceded = 0	# -1pt, FC?
+		# # penaltyMisses = 0		# -2pt
+		# # penaltySaves = 0		# 5pt
+		# # goalsConceded = 0		# -1pt/2goals for GK or MID
+		# yellowCard = False		# -1pt, Y
+		# redCard = False			# -3pt, R
+		# savesByGK = 0			# 1pt/3saves, AB & position is GK
+		# # recoveredBalls = 0		# 1pt/5balls
 
-		# get corresponding feature vector from all_player_features
-		# get feature num to acronym
-		player_ft_values = all_player_features[action.name + "-" + action.team]
-		ft_vector = {}
-		for ft in player_ft:
-			ft_acro = feature_num_to_acro[ft]
-			if ft_acro == 'M':
-				if player_ft[ft] > 0:
-					ft_vector['playedInGame'] = 1
-				if player_ft[ft] >= 60:
-					ft_vector['played60Min'] = 1
-			elif ft_acro == 'G':
-				ft_vector['goalsScored'] = player_ft[ft]
-			elif ft_acro == 'FS':
-				ft_vector['penaltiesReceived'] = player_ft[ft]
-			elif ft_acro == 'FC':
-				ft_vector['penaltiesConceded'] = player_ft[ft]
-			elif ft_acro == 'Y':
-				ft_vector['yellowCard'] = player_ft[ft]
-			elif ft_acro == 'R':
-				ft_vector['redCard'] = player_ft[ft]
-			elif ft_acro == 'AB':
-				if action.position == "GK":
-					ft_vector['penaltiesReceived'] = player_ft[ft] / 3.0
+		# # get corresponding feature vector from all_player_features
+		# # get feature num to acronym
+		# player_ft_values = all_player_features[action.name + "-" + action.team]
+		# ft_vector = {}
+		# for ft in player_ft:
+		# 	ft_acro = feature_num_to_acro[ft]
+		# 	if ft_acro == 'M':
+		# 		if player_ft[ft] > 0:
+		# 			ft_vector['playedInGame'] = 1
+		# 		if player_ft[ft] >= 60:
+		# 			ft_vector['played60Min'] = 1
+		# 	elif ft_acro == 'G':
+		# 		ft_vector['goalsScored'] = player_ft[ft]
+		# 	elif ft_acro == 'FS':
+		# 		ft_vector['penaltiesReceived'] = player_ft[ft]
+		# 	elif ft_acro == 'FC':
+		# 		ft_vector['penaltiesConceded'] = player_ft[ft]
+		# 	elif ft_acro == 'Y':
+		# 		ft_vector['yellowCard'] = player_ft[ft]
+		# 	elif ft_acro == 'R':
+		# 		ft_vector['redCard'] = player_ft[ft]
+		# 	elif ft_acro == 'AB':
+		# 		if action.position == "GK":
+		# 			ft_vector['penaltiesReceived'] = player_ft[ft] / 3.0
 
-		# dot vector feature and weights
-		for ft in ft_vector:
-			reward += weights[ft] * ft_vector[ft]
+		# # dot vector feature and weights
+		# for ft in ft_vector:
+		# 	reward += weights[ft] * ft_vector[ft]
 
-		# TO DO: remove this print
-		print "reward is: ", reward
+		# TO DO: confused. This basically looks like the featureExtractor
+		# for now, let's say reward is -price
+		reward = -1 * action.price
 
 		return [(newState, 1, reward)]
 
@@ -201,7 +221,7 @@ allTeams = {}
 # add players to their corresponding Teams
 for line in lines:
 	last_name, team, position, price = line.rstrip().split(", ")
-
+	price = float(price)
 	team_dict = check_for_accented_key(team, team_to_player_num);
 
 	num = team_dict[last_name]
@@ -260,8 +280,9 @@ for matchday in os.listdir("player_statistics/2015-16/"):
 							if len(features) > 1:
 								for ft in features:
 									ft_num, ft_val = ft.split(":")
+									ft_acro = feature_num_to_acro[ft_num]
 									ft_val = float(ft_val)
-									player_ft[ft_num] = ft_val
+									player_ft[ft_acro] = ft_val
 								def aggregate_player_features(key, team, ft_dict):
 									for ft in ft_dict:
 										all_player_features[key][ft] += ft_dict[ft]
@@ -296,3 +317,7 @@ budget = 100.0
 mdp = ComputeRosterMDP(players, budget, allTeams, allPlayers)
 rl = util.QLearningAlgorithm(mdp.actions, mdp.discount(), util.fantasyFeatureExtractor)
 print "Finished in %s iterations" % rl.numIters
+qRewards = util.simulate(mdp, rl, 100, verbose=True)
+
+
+
