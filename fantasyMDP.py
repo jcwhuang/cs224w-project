f5d2c9831f0830bf622856848c387ddead0d9a44
jcwhuang <jcwhuang@gmail.com>
2015-11-11 11:37:21 -0800

    added stats to Player objects, working on reward

--- fantasyMDP.py
+++ fantasyMDP.py
@@ -9,12 +9,13 @@ import unicodedata
 
 # to compute 15-man roster from available players
 class ComputeRosterMDP(util.MDP):
+
 	def __init__(self, players, budget, allTeams, allPlayers):
+		self.allPlayers = allPlayers
+		self.allTeams=allTeams
 		self.players=players
 		self.budget=budget
-		self.maxPositions = {'GK': 2, 'DEF': 5, 'MID': 5, 'STR': 3}
-        # self.allTeams=allTeams
-        # self.allPlayers = allPlayers
+		self.maxPositions={'GK': 2, 'DEF': 5, 'MID': 5, 'STR': 3}
 
 	# a state = (roster, budget, positions, teams)
 	# roster -- a list of class Player that represents the players we have 
@@ -42,10 +43,7 @@ class ComputeRosterMDP(util.MDP):
 	# 6) Can select up to 3 players from a single Premier League team
 	def actions(self,state):
 		actions = []		
-		# TO DO: how to store all players? 
-		#       There are duplicate names from different teams :(
-		#       The alternative is to use the dict team->player name -> playernum
-		for p in AllPlayers:
+		for p in self.allPlayers:
 			if p not in state[0]: #if not in roster yet
 				positionConstraint = (state.positions[p.position] < self.maxPositions[p.position])
 				budgetConstraint = (p.price < state.budget)
@@ -53,8 +51,7 @@ class ComputeRosterMDP(util.MDP):
 				if positionConstraint and budgetConstraint and teamConstraint:
 					actions.append(p)
 		return actions
-					
-				
+
 	def succAndProbReward(self, state, action):
 		# if roster is full
 		if len(state[0]) == 15:
@@ -70,33 +67,77 @@ class ComputeRosterMDP(util.MDP):
 		newTeams[action.team] += 1
 		newState = (newRoster, newBudget, newPositions, newTeams)
 
-		# TO DO: Reward function
+		# TO DO: Finish reward function
 		reward = 0 
-		playedInGame = False 	# 1pt
-		played60Min = False		# 2pt
-		goalsScored = 0			# 6pt if GK or DEF, 5pt if MID, 4 if STR
-		assists = 0				# 3pt
-		noScoreOnGK = False		# 4pt if played 60min
-		noScoreOnDEF = False	# 4pt if played 60min
-		noScoreOnMID = False	# 1pt if played 60min
-		penaltiesReceived = 0	# 1pt 
-		penaltiesConceded = 0	# -1pt 
-		penaltyMisses = 0		# -2pt
-		penaltySaves = 0		# 5pt
-		goalsConceded = 0		# -1pt/2goals for GK or MID
-		yellowCard = False		# -1pt
-		redCard = False			# -3pt
-		savesByGK = 0			# 1pt/3saves
-		recoveredBalls = 0		# 1pt/5balls
+		goalsScoredWeight = 0
+		if action.position == "GK" or action.position == "DEF":
+			goalsScoredWeight = 6
+		elif action.position == "MID":
+			goalsScoredWeight = 5
+		else:
+			goalsScoredWeight = 4
 
+		weights={'playedInGame': 1, 'played60Min': 2, 'goalsScored': goalsScoredWeight, \
+			'penaltiesReceived' : 1, 'penaltiesConceded': -1, \
+			'goalsConceded' : -1, 'yellowCard' : -1, 'redCard':-3, \
+			'savesByGK': 1}
 
+		# commented out features that we may not have stats for
+		# TO DO: some of these are in team stats. Should we use that?
+
+		playedInGame = False 	# 1pt, M > 0
+		played60Min = False		# 2pt, M >= 60
+		goalsScored = 0			# 6pt if GK or DEF, 5pt if MID, 4 if STR, G & position
+		# assists = 0				# 3pt
+		# noScoreOnGK = False		# 4pt if played 60min
+		# noScoreOnDEF = False	# 4pt if played 60min
+		# noScoreOnMID = False	# 1pt if played 60min
+		penaltiesReceived = 0	# 1pt, FS?
+		penaltiesConceded = 0	# -1pt, FC?
+		# penaltyMisses = 0		# -2pt
+		# penaltySaves = 0		# 5pt
+		# goalsConceded = 0		# -1pt/2goals for GK or MID
+		yellowCard = False		# -1pt, Y
+		redCard = False			# -3pt, R
+		savesByGK = 0			# 1pt/3saves, AB & position is GK
+		# recoveredBalls = 0		# 1pt/5balls
+
+		# get corresponding feature vector from all_player_features
+		# get feature num to acronym
+		player_ft_values = all_player_features[action.name + "-" + action.team]
+		ft_vector = {}
+		for ft in player_ft:
+			ft_acro = feature_num_to_acro[ft]
+			if ft_acro == 'M':
+				if player_ft[ft] > 0:
+					ft_vector['playedInGame'] = 1
+				if player_ft[ft] >= 60:
+					ft_vector['played60Min'] = 1
+			elif ft_acro == 'G':
+				ft_vector['goalsScored'] = player_ft[ft]
+			elif ft_acro == 'FS':
+				ft_vector['penaltiesReceived'] = player_ft[ft]
+			elif ft_acro == 'FC':
+				ft_vector['penaltiesConceded'] = player_ft[ft]
+			elif ft_acro == 'Y':
+				ft_vector['yellowCard'] = player_ft[ft]
+			elif ft_acro == 'R':
+				ft_vector['redCard'] = player_ft[ft]
+			elif ft_acro == 'AB':
+				if action.position == "GK":
+					ft_vector['penaltiesReceived'] = player_ft[ft] / 3.0
+
+		# dot vector feature and weights
+		for ft in ft_vector:
+			reward += weights[ft] * ft_vector[ft]
+
+		# TO DO: remove this print
+		print "reward is: ", reward
 
 		return [(newState, 1, reward)]
 
 	def discount(self):
 		return 1
-
-
 
 class ComputeMatchLineupMDP(util.MDP):
 	def __init__(self, roster, budget):
@@ -204,7 +245,6 @@ for matchday in os.listdir("player_statistics/2015-16/"):
 				# use key: player_num-team
 				# for each team, keep a sum of how many games played
 
-
 				# get team name
 				m = re.match("^.*-(.*)$", tf)
 				if m:
@@ -232,20 +272,27 @@ for matchday in os.listdir("player_statistics/2015-16/"):
 # normalize player features over num games played
 for key in all_player_features:
 	team = re.sub(".*-", "", key)
-	for player in all_player_features[key]:
-		all_player_features[key][player] /= team_to_num_games[team]
-	print "%s: " % key, all_player_features[key]
+	for ft in all_player_features[key]:
+		all_player_features[key][ft] /= team_to_num_games[team]
 
-				
+# attach these stats to Player objects in allPlayers
+for player in allPlayers:
+	last_name, num, team = player.split("-", 2)
+	allPlayers[player].stats = all_player_features[num + "-" + team]
 
-'''for p in players.keys():
-	print players[p].name
-	print players[p].team
-	print players[p].position
-	print players[p].price
+'''			
+for p in allPlayers.keys():
+	print "------------"
+	print allPlayers[p].name
+	print allPlayers[p].team
+	print allPlayers[p].position
+	print allPlayers[p].price
+	print allPlayers[p].stats
+	print "------------"
 '''
 
-'''budget = 100.0
-mdp = ComputeRosterMDP(players, budget)
+
+budget = 100.0
+mdp = ComputeRosterMDP(players, budget, allTeams, allPlayers)
 rl = util.QLearningAlgorithm(mdp.actions, mdp.discount(), util.fantasyFeatureExtractor)
-'''
+print "Finished in %s iterations" % rl.numIters
